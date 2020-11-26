8fc43ce6457d77c1aec0a82374850865869dfb85
Anders Kristiansen <anders.kristiansen@virinco.com>
2020-11-26 09:12:47 +0100

    Initial commit

---
+++ ConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+using Virinco.WATS.Interface;
+using System.IO;
+
+namespace ListenincConverters
+{
+    [TestClass]
+    public class ConverterTests : TDM
+    {
+        [TestMethod]
+        public void SetupClient()
+        {
+            SetupAPI(null, "Drammen, Norway", "Demo", true);
+            RegisterClient("https://listen.skywats.com", "", "cmVnaXN0ZXJDbGllbnRUb2tlbjp0OHQzZXpycXJscGEzMTR3dHNydTJy");
+            InitializeAPI(true);
+        }
+
+        [TestMethod]
+        public void TestSoundCheckConverter()
+        {
+            InitializeAPI(true);
+            SoundCheckConverter converter = new SoundCheckConverter();
+            using (FileStream file = new FileStream("Data\\WATS18 11-2-2020.txt", FileMode.Open))
+            {
+                Report uut = converter.ImportReport(this, file);
+                Submit(uut);
+            }
+        }
+    }
+}
