e1535d745181c885a56236acf65c5db1e771c9b4
Anders Kristiansen <anders.kristiansen@virinco.com>
2020-11-26 09:12:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+﻿# WATS Client Converter - Listen
+
+A WATS Client converter plugin for importing test data from Listen's SoundCheck test software to WATS.
+
+## Getting Started
+
+* [About WATS](https://wats.com/manufacturing-intelligence/)
+* [About submitting data to WATS](https://virinco.zendesk.com/hc/en-us/articles/207424613)
+* [About creating custom converters](https://virinco.zendesk.com/hc/en-us/articles/207424593)
+
+## Download
+
+You can download the latest released version of the converter [here](https://github.com/Virinco/WATS-Client-Converter-Listen/releases/latest). See the Custom Converter section in the [WATS Client Installation Guide](https://wats.com/download) for your version of the WATS Client for how to install a converter.
+
+### Parameters
+
+This converter uses the following parameters:
+
+| Parameter         | Default value | Description                                                    |
+|-------------------|---------------|----------------------------------------------------------------|
+| partNumber        | ABC123        | If log is missing a part number, use this one.                 |
+| partRevision      | 1.0           | If log is missing a revision, use this one.                    |
+| sequenceName      | MainSequence  | If log is missing sequence name, use this one.                 |
+| sequenceVersion   | 1.0.0         | If log is missing sequence version, use this one.              |
+| operationTypeCode | 10            | If log is missing operation code (process code), use this one. |
+
+## Testing
+
+The project uses the [MSTest framework](https://docs.microsoft.com/en-us/visualstudio/test/quick-start-test-driven-development-with-test-explorer) for testing the converter.
+
+It is setup with two tests; one for setting up the API by registering the client to your WATS, and one for running the converter.
+
+The values are hardcoded in the test, so you will need to change the values to reflect your setup.
+* In SetupClient, fill in your information in the the call to RegisterClient.
+* In TestConverter, fill in the path to the file you want to test the converter with. There are example files in the Examples folder.
+* Run SetupClient once, then you can run TestConverter as many times as you want.
+
+## Contributing
+
+We're open to suggestions! Feel free open an issue or create a pull request.
+
+Please read [Contributing](CONTRIBUTING.md) for details on contributions.
+
+## Contact
+
+* Issues with the converter or suggestions for improvements can be submitted as an issue here on GitHub.
+* Ask questions about WATS in the [WATS Community Help](https://virinco.zendesk.com/hc/en-us/community/topics/200229613)
+* Suggestions for the WATS Client itself or WATS in general can be submitted to the [WATS Idea Exchange](https://virinco.zendesk.com/hc/en-us/community/topics/200229623)
+* Sensitive installation issues or other sensitive questions can be sent to [support@virinco.com](mailto://support@virinco.com)
+
+## License
+
+This project is licensed under the [LGPLv3](COPYING.LESSER) which is an extention of the [GPLv3](COPYING).
---
+++ SoundCheckConverter.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.IO;
+using Virinco.WATS.Interface;
+
+namespace ListenincConverters
+{
+    public class SoundCheckConverter : IReportConverter_v2
+    {
+        Dictionary<string, string> parameters;
+        public SoundCheckConverter() : base()
+        {
+            parameters = new Dictionary<string, string>()
+            {
+                {"partRevision","1.0" },
+                {"operationTypeCode","700" },
+                {"operator","oper" },
+                {"sequenceName","SoundCheckSeq" },
+                {"sequenceVersion","1.0.0" },
+            };
+        }
+
+        public Dictionary<string, string> ConverterParameters => parameters;
+
+        public void CleanUp()
+        {
+        }
+
+        public Report ImportReport(TDM api, Stream file)
+        {
+            UUTReport unitTestReport = null;
+            using (TextReader reader = new StreamReader(file))
+            {
+                //Serial #:	Time: 	Lot #: 	Name	Margin	Unit	Result	Tolerance	Limits
+                string header = reader.ReadLine();
+                string line = reader.ReadLine();
+                string sn="",measureName="", xUnit="", yUnit="";
+                double[] xValues = null;
+                while (line != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        string[] columns = line.Split(new char[] { '\t' });
+                        if (columns[0]!="Serial #:")
+                            sn = columns[0];
+                        if (unitTestReport == null || unitTestReport.SerialNumber != sn)
+                        {
+                            if (unitTestReport != null)
+                                api.Submit(unitTestReport);
+                            unitTestReport = CreateUUTFromHeader(api, columns, sn);
+                        }
+                        if (columns[0] == "Serial #:")
+                            ReadGraphHeader(unitTestReport, columns, out measureName, out xUnit, out yUnit);
+                        else if (columns.Length > 10)
+                            ReadGraphData(unitTestReport, columns, measureName, xUnit, yUnit, ref xValues);
+                        else
+                            ReadMeasure(unitTestReport, columns);
+                    }
+                    line = reader.ReadLine();
+                }
+                if (unitTestReport != null)
+                    api.Submit(unitTestReport);
+            }
+            return unitTestReport;
+        }
+
+        private void ReadGraphData(UUTReport unitTestReport, string[] columns, string measureName, string xUnit, string yUnit, ref double[] xValues)
+        {
+            List<double> values = new List<double>();
+            for (int i = 3; i < columns.Length; i++)
+                values.Add(double.Parse(columns[i], CultureInfo.InvariantCulture));
+            if (xValues == null)
+            {
+                xValues = values.ToArray();
+                return;
+            }
+            NumericLimitStep numericLimitStep = unitTestReport.GetRootSequenceCall().AddNumericLimitStep(measureName);
+            Chart chart = numericLimitStep.AddChart(ChartType.LineLogY, measureName, "X", xUnit, "Y", yUnit);
+            chart.AddSeries(measureName, xValues, values.ToArray());
+            xValues = null;
+        }
+
+        private void ReadGraphHeader(UUTReport unitTestReport, string[] columns, out string measureName, out string xUnit, out string yUnit)
+        {
+            measureName = columns[3];
+            xUnit = columns[13];
+            yUnit = columns[14];
+            ;
+        }
+
+        private void ReadMeasure(UUTReport unitTestReport, string[] columns)
+        {
+            NumericLimitStep numericLimitStep = unitTestReport.GetRootSequenceCall().AddNumericLimitStep(columns[3]);
+            double measure = double.Parse(columns[4], CultureInfo.InvariantCulture);
+            if (columns[8].Contains("/"))
+            {
+                string[] lim = columns[8].Split(new char[] { '/' });
+                double lowLim = double.Parse(lim[1], CultureInfo.InvariantCulture);
+                double hiLim = double.Parse(lim[0], CultureInfo.InvariantCulture);
+                numericLimitStep.AddTest(measure, CompOperatorType.GELE, lowLim, hiLim, columns[5]);
+            }
+            else
+                numericLimitStep.AddTest(measure, columns[5]);
+            numericLimitStep.Status = StepStatus(columns[6]);
+        }
+
+        private UUTReport CreateUUTFromHeader(TDM api, string[] cols, string sn)
+        {
+            UUTReport uut=api.CreateUUTReport(parameters["operator"], 
+                cols[2], parameters["partRevision"], 
+                sn, parameters["operationTypeCode"], 
+                parameters["sequenceName"], parameters["sequenceVersion"]);
+            uut.StartDateTime = DateTime.ParseExact(cols[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture); //11/2/2020 4:36:07 PM
+            return uut;
+        }
+
+        StepStatusType StepStatus(string input)
+        {
+            return 
+                input.ToLower() == "passed" ||
+                input.ToLower() == "pass" ? StepStatusType.Passed :
+                input.ToLower() == "failed" ||
+                input.ToLower() == "fail" ? StepStatusType.Failed :
+                input.ToLower() == "terminated" ? StepStatusType.Terminated :
+                input.ToLower() == "error" ? StepStatusType.Error :
+                input.ToLower() == "done" ? StepStatusType.Done :
+                input.ToLower() == "skipped" ? StepStatusType.Skipped : StepStatusType.Error;
+        }
+    }
+}
