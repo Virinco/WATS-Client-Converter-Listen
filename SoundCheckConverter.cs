a657917b7697d3fb40d5c5f9c435321d7b9749b1
rick-spada <40271490+rick-spada@users.noreply.github.com>
2021-01-19 08:32:34 -0500

    take into account local culture settings for timestamps and parsing doubles

--- SoundCheckConverter.cs
+++ SoundCheckConverter.cs
@@ -70,7 +70,7 @@ namespace ListenincConverters
         {
             List<double> values = new List<double>();
             for (int i = 3; i < columns.Length; i++)
-                values.Add(double.Parse(columns[i], CultureInfo.InvariantCulture));
+                values.Add(double.Parse(columns[i], CultureInfo.CurrentUICulture));
             if (xValues == null)
             {
                 xValues = values.ToArray();
@@ -112,7 +112,11 @@ namespace ListenincConverters
                 cols[2], parameters["partRevision"], 
                 sn, parameters["operationTypeCode"], 
                 parameters["sequenceName"], parameters["sequenceVersion"]);
-            uut.StartDateTime = DateTime.ParseExact(cols[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture); //11/2/2020 4:36:07 PM
+            try {
+                uut.StartDateTime = DateTime.ParseExact(cols[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture); //11/2/2020 4:36:07 PM
+            } catch (System.FormatException) {
+                uut.StartDateTime = DateTime.ParseExact(cols[1], "M-d-yyyy h:mm:ss", CultureInfo.InvariantCulture);
+            }
             return uut;
         }
 
